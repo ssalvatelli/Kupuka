be29a32173d382555d4e5f395324201f4fa9d1f1
Santiago Salvatelli <santiago_salvatelli@hotmail.com>
2023-10-04 21:09:56 -0300

    Actividad1_Salvatelli

---
+++ src/components/commons/itemCount/ItemCount.jsx
@@ -0,0 +1,36 @@
+import "./ItemCount.css";
+import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import IconButton from "@mui/material/IconButton";
+import RemoveIcon from "@mui/icons-material/Remove";
+
+function ItemCount({ stock, onAdd }) {
+  const [count, setCount] = useState(1);
+
+  function decrement() {
+    if (count > 1) setCount(count - 1);
+  }
+
+  function increment() {
+    if (count < stock) setCount(count + 1);
+  }
+
+  return (
+    <div className="counter">
+      <div className="controls">
+        <IconButton onClick={decrement} size="small">
+          <RemoveIcon color="primary" fontSize="small" />
+        </IconButton>
+        <h4>{count}</h4>
+        <IconButton onClick={increment} size="small">
+          <AddIcon color="primary" fontSize="small" />
+        </IconButton>
+      </div>
+      <button onClick={() => onAdd(count)} disabled={!stock}>
+        Agregar al carrito
+      </button>
+    </div>
+  );
+}
+
+export default ItemCount;
--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,7 +1,13 @@
+import ItemCount from "../../commons/itemCount/ItemCount";
+
 function ItemListContainer({ greeting }) {
   return (
     <div className="card">
       <h1>{greeting}</h1>
+      <ItemCount
+        stock={23}
+        onAdd={(count) => console.log("Cantidad agregada:", count)}
+      />
     </div>
   );
 }
