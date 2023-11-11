a805043416acab1ab851619f3eb1bb11cba37e9b
Santiago Salvatelli <santiago_salvatelli@hotmail.com>
2023-11-11 06:10:26 -0300

    EntregaFinal_Salvatelli

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Cart from "./components/pages/cart/Cart";
 import CartProvider from "./context/CartContext";
 import Navbar from "./components/layouts/navbar/Navbar";
-import Checkout from "./components/pages/Checkout/Checkout";
+import Checkout from "./components/pages/checkout/Checkout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/pages/itemDetailContainer/ItemDetailContainer";
